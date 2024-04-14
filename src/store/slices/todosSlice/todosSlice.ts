9052cb598af4b41e842d5e8db26e40e054cdf4f1
Alexandr-Shtyb <styb701@mail.ru>
2024-04-14 17:32:33 +0300

    feat: implemented optional functionality

--- src/store/slices/todosSlice/todosSlice.ts
+++ src/store/slices/todosSlice/todosSlice.ts
@@ -1,18 +1,25 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
-import type { Todo } from "types";
+import type { Todo, TodoFilters } from "types";
+import type { DropResult } from "react-beautiful-dnd";
 
 interface TodosState {
   todos: Todo[];
   isOpenModalForm: boolean;
+  isOpenEditModalForm: boolean;
+  idEditedTask: string;
+  currentFilter: TodoFilters;
 }
 
 const initialState: TodosState = {
-  todos: [
+  todos: JSON.parse(localStorage.getItem("todos") as string) || [
     { id: "11", title: "Task1", status: false },
     { id: "2222", title: "Task2", status: true },
   ],
   isOpenModalForm: false,
+  isOpenEditModalForm: false,
+  idEditedTask: "",
+  currentFilter: "All",
 };
 
 export const todosSlice = createSlice({
@@ -25,12 +32,14 @@ export const todosSlice = createSlice({
 
       if (todoToUpdate) {
         todoToUpdate.status = !todoToUpdate.status;
+        localStorage.setItem("todos", JSON.stringify(state.todos));
       }
     },
     deleteTodo(state, action: PayloadAction<string>) {
       const todoId = action.payload;
 
       state.todos = state.todos.filter(todo => todo.id !== todoId);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
     addTask(state, action: PayloadAction<string>) {
       const titleTodo = action.payload;
@@ -42,9 +51,36 @@ export const todosSlice = createSlice({
       };
 
       state.todos.push(newTodo);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
+    },
+    editTask(state, action: PayloadAction<{ id: string; title: string }>) {
+      const { id, title } = action.payload;
+      const todoToUpdate = state.todos.find(todo => todo.id === id);
+
+      if (todoToUpdate) {
+        todoToUpdate.title = title;
+        localStorage.setItem("todos", JSON.stringify(state.todos));
+      }
     },
     changeStatusModalForm(state) {
       state.isOpenModalForm = !state.isOpenModalForm;
+    },
+    changeStatusEditModalForm(state) {
+      state.isOpenEditModalForm = !state.isOpenEditModalForm;
+    },
+    setIdEditedTask(state, action: PayloadAction<string>) {
+      state.idEditedTask = action.payload;
+    },
+    setFilterTodo(state, action: PayloadAction<TodoFilters>) {
+      state.currentFilter = action.payload;
+    },
+    onDragEnd(state, action: PayloadAction<DropResult>) {
+      const result = action.payload;
+      if (!result.destination) return;
+
+      const [reorderItem] = state.todos.splice(result.source.index, 1);
+      state.todos.splice(result.destination.index, 0, reorderItem);
+      localStorage.setItem("todos", JSON.stringify(state.todos));
     },
   },
 });
